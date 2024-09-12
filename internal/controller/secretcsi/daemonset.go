319c67429c35867b4d456cc8f0126e172549b8da
whg517 <huagang517@126.com>
2024-09-12 10:42:43 +0800

    test(e2e): add catch statement to show log when failure

--- internal/controller/secretcsi/daemonset.go
+++ internal/controller/secretcsi/daemonset.go
@@ -113,7 +113,7 @@ func (r *DaemonSet) getVolumes() []corev1.Volume {
 func (r *DaemonSet) makeDaemonset() (*appv1.DaemonSet, error) {
 	labels := map[string]string{
 		"app.kubenetes.io/name":        "secret-csi",
-		"app.kubernetes.io/instance":   r.cr.GetName(),
+		"app.kubernetes.io/instance":   r.getName(),
 		"app.kubernetes.io/part-of":    "secret-csi",
 		"app.kubernetes.io/managed-by": "secret-operator",
 		"app.kubernetes.io/created-by": "secret-operator",
